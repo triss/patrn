66dc42d92f2c3828fcd1d948678c28be12ffe917
Tristan Strange <tristan.strange@gmail.com>
2016-01-12 16:24:09 +0000

    Fixed event playback issue for Overtone events. #2

--- src/patrn/overtone.clj
+++ src/patrn/overtone.clj
@@ -1,6 +1,7 @@
 (ns patrn.overtone
-  "Functions to simplify the playback of patrns with Overtone."
-  (:require [patrn.event :as event]
+  "Functions to simplify the playback of event sequences with Overtone."
+  (:require [clojure.pprint :as pprint] 
+            [patrn.event :as event]
             [patrn.core :as p])
   (:use [overtone.core]))
 
@@ -11,34 +12,30 @@
 (defn close-synth-gate
   "Closes synth's gate control at time t on metronome."
   [synth {:keys [length time-stamp metronome]}] 
-  (at (metronome (+ time-stamp length)) 
+  (at (metronome (+ time-stamp (or length 1))) 
       (ctl synth :gate 0)))
+
+(defn pp [x] (pprint/pprint x) x)
 
 (defn play-derived-event
   "Plays a single event with Overtone instrument specified within it."
-  [{:keys [instrument metronome time-stamp] 
-    :or   {time-stamp (metronome)} 
-    :as   event}]
+  [{:keys [instrument metronome time-stamp] :as event}]
   (assert instrument "Event :instrument key value must be specified.")
   (at (metronome time-stamp) 
       (let [inst-params (inst-param-names instrument)
-            used-params ((apply juxt inst-params) event)
-            synth       (apply instrument used-params)]
-        (when (#{:gate} inst-params)
+            used-params (select-keys event inst-params)
+            synth       (apply instrument (flatten (seq used-params)))]
+        (when (some #{:gate} inst-params)
           (close-synth-gate synth event))
         synth)))
 
-(def default-metronome (metronome 60))
+(def default-metronome 
+  "Default metronome to playback events on."
+  (metronome 60))
 
 (def base-event 
   "Provides default set of event value derivations for use with Overtone."
   (assoc event/default-event :metronome default-metronome))
-
-(def play-event
-  "Helper method for playing overtone event maps."
-  (comp play-derived-event 
-        event/derive-vals
-        #(merge base-event %)))
 
 (defn ensure-time-stamped
   "Ensures event has a time stamp. 
@@ -47,7 +44,15 @@
   (let [ts (or time-stamp (if metronome (metronome) (default-metronome)))] 
     (assoc event :time-stamp ts)))
 
+(def play-event
+  "Helper method for playing overtone event maps."
+  (comp play-derived-event 
+        event/derive-vals
+        ensure-time-stamped
+        #(merge base-event %)))
+
 (defn time-stamp-events
+  "Calculate time-stamp for each event in sequence if not already specified."
   [events]
   (->> events
        (reductions (fn [prev-ts {:keys [time-stamp duration] 
